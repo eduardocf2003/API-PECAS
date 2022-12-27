2dd20e57909eebcbc9dba8ad499de15815b774c8
Eduardo <florianoeduardo03@gmail.com>
2022-12-26 21:37:45 -0300

    Commit Final

---
+++ API-PECAS/src/main/java/br/com/mesttra/apipecas/ApiPecasApplication.java
@@ -0,0 +1,13 @@
+package br.com.mesttra.apipecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPecasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPecasApplication.class, args);
+    }
+
+}
---
+++ API-PECAS/src/test/java/br/com/mesttra/apipecas/ApiPecasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mesttra.apipecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPecasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
